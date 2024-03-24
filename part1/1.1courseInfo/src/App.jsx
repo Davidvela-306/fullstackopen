f4eee1b45f53961e3e7cdd4c71d4ffe618d2c45b
David Vela <david.vela@epn.edu.ec>
2024-03-23 20:48:16 -0500

    task 1.3 added in 1.1courseInfo directory

--- part1/1.1courseInfo/src/App.jsx
+++ part1/1.1courseInfo/src/App.jsx
@@ -3,63 +3,40 @@ import Header from "./header"
 import Content from "./content";
 import Total from "./Total";
 
-// const App = () => {
-//   const course = 'Desarrollo de aplicación Half  Stack';
-//   const part1 = 'Fundamentos de React'
-//   const excercises1 = 10
-//   const part2 = 'Usando props para pasar datos'
-//   const excercises2 = 7
-//   const part3 = 'Estado de un componente'
-//   const excercises3 = 14
-//   return(
-//     <>
-//       <Header course = {course}/>
-//       <Content 
-//       part1 = {part1}
-//       part2 = {part2}
-//       part3 = {part3}
-//       exercises1={excercises1}
-//       exercises2={excercises2}
-//       exercises3={excercises3}
-//       />
-//       <Total 
-//       resultado = {excercises1+excercises2+excercises3}    
-//       />
-//     </>
-//   )
-// }
-const App = () =>{
-  const course ={
-    name :'Half Stack application development',
-    parts :[
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name:'User Props to pass-data',
-        exercises: 7
-      },
-      {
-        name:'State of a component',
-        exercises: 14
-      }
-    ]
+const App = () => {
+  const course = 'Half Stack application developent'
+  const part1 = {
+    name : 'Fundamentals of React',
+    excercises : 10
+  }
+  const part2 = {
+    name : 'Using props to pass data',
+    excercises : 17
+  }
+  const part3 = {
+    name : 'State of a component',
+    excercises : 14
   }
 
   return(
     <>
-      <Header course = {course['name']}/>
+      <Header course = {course}/>
       <Content 
-      part1 = {course.parts[0].name}
-      part2 = {course.parts[1].name}
-      part3 = {course.parts[2].name}
+      part1 = {part1.name}
+      exercises1 = {part1.excercises}
       
-      exercises1 = {course.parts[0].exercises}
-      exercises2 = {course.parts[1].exercises}
-      exercises3 = {course.parts[2].exercises}
+      part2= {part2.name}
+      exercises2={part2.excercises}
+
+      part3= {part3.name}
+      exercises3={part3.excercises} 
       />
+      <Total 
+      resultado = {part1.excercises + part2.excercises + part3.excercises}
+      />
+      
     </>
   )
 }
+
 export default App
