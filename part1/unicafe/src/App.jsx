dcb8e72310ac33831fb92c6c1075a55c7c878c29
David Vela <david.vela@epn.edu.ec>
2024-04-20 23:44:35 -0500

    1.8: unicafe, step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,64 +1,53 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Button = (props) =>{
-  return(
-    <button onClick={props.handleClick}>{props.text}</button>
-  )
-}
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+// un lugar adecuado para definir un componente
+const Statistics = (props) => {
+  console.log(props);
+
+  return (
+    <p>
+      {props.text}: {props.statistic}
+    </p>
+  );
+};
 
 const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [averange, setAverange] = useState(0)
-  const [positivePercent, setPositivePercent] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleGoodClick = () => {
-    setGood(good+1)
-    setAll(all+1)
-    const goodVar = good+1
-    const allVar = all+1
-    setAverange((goodVar*1 + neutral*0 + bad*-1)/allVar)
-    setPositivePercent((goodVar/allVar)*100)
-    
-  }
+    setGood(good + 1);
+  };
   const handleNeutralClick = () => {
-    setNeutral(neutral+1)
-    setAll(all+1)
-    const neutralVar = neutral+1
-    const allVar = all+1
-    setAverange((good*1 + neutralVar*0 + bad*-1)/allVar)
-    setPositivePercent((good/allVar)*100)
-
-  }
+    setNeutral(neutral + 1);
+  };
   const handleBadClick = () => {
-    setBad(bad+1)
-    setAll(all+1)
-    const badVar = bad+1
-    const allVar = all+1
-    setAverange((good*1 + neutral*0 + badVar*-1)/allVar)
-    setPositivePercent((good/allVar)*100)
-    
-  }
+    setBad(bad + 1);
+  };
+  const total = good + bad + neutral;
+  const positivePercent = total === 0 ? 0 : (good / total) * 100;
+  const average = total === 0 ? 0 : (good - bad) / total;
 
   return (
     <>
       <h1>give feedback</h1>
-      <Button handleClick={handleGoodClick} text={'good'}/>
-      <Button handleClick={handleNeutralClick} text={'neutral'}/>
-      <Button handleClick={handleBadClick} text={'bad'}/>
+      <Button handleClick={handleGoodClick} text={"good"} />
+      <Button handleClick={handleNeutralClick} text={"neutral"} />
+      <Button handleClick={handleBadClick} text={"bad"} />
       <h1>statics</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {all}</p>
-      <p>averange: {averange}</p>
-      <p>positive: {positivePercent}%</p>
-      
+      <Statistics statistic={good} text={"good"} />
+      <Statistics statistic={neutral} text={"neutral"} />
+      <Statistics statistic={bad} text={"bad"} />
+      <Statistics statistic={total} text={"all"} />
+      <Statistics statistic={average} text={"average"} />
+      <Statistics statistic={positivePercent} text={"positivePercent"} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
