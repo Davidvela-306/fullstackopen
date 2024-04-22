4615ee4a06ef67de9c17731f111baac47bf428f2
David Vela <david.vela@epn.edu.ec>
2024-04-22 18:57:28 -0500

    1.9: unicafe paso 4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,7 +7,6 @@ const Button = (props) => {
 // un lugar adecuado para definir un componente
 const Statistics = (props) => {
   console.log(props);
-
   return (
     <p>
       {props.text}: {props.statistic}
@@ -19,15 +18,19 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [feedbackGiven, setFeedbackGiven] = useState(false);
 
   const handleGoodClick = () => {
     setGood(good + 1);
+    setFeedbackGiven(true);
   };
   const handleNeutralClick = () => {
     setNeutral(neutral + 1);
+    setFeedbackGiven(true);
   };
   const handleBadClick = () => {
     setBad(bad + 1);
+    setFeedbackGiven(true);
   };
   const total = good + bad + neutral;
   const positivePercent = total === 0 ? 0 : (good / total) * 100;
@@ -39,13 +42,19 @@ const App = () => {
       <Button handleClick={handleGoodClick} text={"good"} />
       <Button handleClick={handleNeutralClick} text={"neutral"} />
       <Button handleClick={handleBadClick} text={"bad"} />
-      <h1>statics</h1>
-      <Statistics statistic={good} text={"good"} />
-      <Statistics statistic={neutral} text={"neutral"} />
-      <Statistics statistic={bad} text={"bad"} />
-      <Statistics statistic={total} text={"all"} />
-      <Statistics statistic={average} text={"average"} />
-      <Statistics statistic={positivePercent} text={"positivePercent"} />
+      {feedbackGiven ? (
+        <>
+          <h1>statics</h1>
+          <Statistics statistic={good} text={"good"} total={total} />
+          <Statistics statistic={neutral} text={"neutral"} total={total} />
+          <Statistics statistic={bad} text={"bad"} total={total} />
+          <Statistics statistic={total} text={"all"} total={total} />
+          <Statistics statistic={average} text={"average"} total={total} />
+          <Statistics statistic={positivePercent} text={"positivePercent"} />
+        </>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </>
   );
 };
