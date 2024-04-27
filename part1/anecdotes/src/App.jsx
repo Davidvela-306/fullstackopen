0a0867fd22ff90734b0b9861ead20828578a3422
David Vela <david.vela@epn.edu.ec>
2024-04-27 18:22:05 -0500

    1.14: anecdotes, step 3

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -4,28 +4,40 @@ import { useState } from "react";
 function getRandomNumber(array) {
   //random será un núm entre 0 y 1
   const random = Math.random();
-  //tamaño, empezando por 1, del arr
+  //tamaño, empezando por 1, del arrVotes
   const lengthArr = array.length; //reducimos en uno por los índices
-  //Multiplicamos para aumentar el rango del número entero hastá la logitud del arr
+  //Multiplicamos para aumentar el rango del número entero hastá la logitud del arrVotes
   const multiplied = random * lengthArr;
-  //sacamos solo los números enteros desde 0 hasta la longitud del arr
+  //sacamos solo los números enteros desde 0 hasta la longitud del arrVotes
   const result = Math.floor(multiplied);
 
   // Asegurar que el resultado esté dentro del rango válido de índices
   return result < lengthArr ? result : lengthArr - 1;
 }
-//crear un arr lleno de 0, de longitud acorde a otro arr como parámetro
+//crear un arrVotes lleno de 0, de longitud acorde a otro arrVotes como parámetro
 function getVote(array) {
   const lengthArr = array.length;
   let newVotesArr = new Array(lengthArr);
   return newVotesArr.fill(0);
 }
-
-//*ARREGLOS
+function getIndexMaxVote(arrVotes) {
+  const mayor = Math.max(...arrVotes); // Obtiene el valor máximo del array
+  const index = arrVotes.indexOf(mayor); // Obtiene el índice del valor máximo
+  return index;
+}
 
 //*COMPONENTES
 const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
+};
+const MostVoted = ({ arrVotes, arrAnecdotes }) => {
+  const maxValIndex = getIndexMaxVote(arrVotes);
+  return (
+    <>
+      <p>{arrAnecdotes[maxValIndex]}</p>
+      <p>votos: {arrVotes[maxValIndex]}</p>
+    </>
+  );
 };
 
 const App = () => {
@@ -38,11 +50,14 @@ const App = () => {
     "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
     "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
     "The only way to go fast, is to go well.",
+    "Soy nuevo xd",
   ];
 
-  const [selected, setSelected] = useState(0);
+  //* ESTADOS
+  const [selected, setSelected] = useState(0); //elegir anecdota aleatoria
   let arrVotes = getVote(anecdotes);
-  const [votes, setVotes] = useState(arrVotes);
+  const [votes, setVotes] = useState(arrVotes); //votos para cada a necdota
+  const [feedBack, setFeedBack] = useState(false); //condicional para mostrar más votada
 
   const handleNextClick = () => {
     const randomNumber = getRandomNumber(anecdotes);
@@ -52,13 +67,25 @@ const App = () => {
     const copyVotes = [...votes];
     copyVotes[selected] = copyVotes[selected] + 1;
     setVotes(copyVotes);
+    setFeedBack(true);
   };
   return (
     <>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>Has {votes[selected]} votes</p>
       <Button handleClick={handleVoteClick} text="vote" />
       <Button handleClick={handleNextClick} text="Next anecdote" />
+      {feedBack ? (
+        <>
+          <h1>Anecdote with most votes</h1>
+          <MostVoted arrVotes={votes} arrAnecdotes={anecdotes} />
+        </>
+      ) : (
+        <>
+          <h1>Dont exist votes</h1>
+        </>
+      )}
     </>
   );
 };
