d2f839ecacf09217b7ef9cb8edf1583cf63583dd
David Vela <david.vela@epn.edu.ec>
2024-06-07 08:02:57 -0500

    2.9: Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,33 +1,58 @@
 import { useState } from "react";
-import Note from "./components/Note"
-
+import Note from "./components/Note";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ id: 1, name: "Arto Hellas", number: "123456" }]);
+  const [allPersons, setAllPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [persons, setPersons] = useState(allPersons); //Se crea una copia para que persistan los datos
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [searchPersons, setSearchPerson] = useState("");
 
   const handleChangeName = (event) => {
-    console.log("teclado: ", event.target.value);
     setNewName(event.target.value);
   };
+
   const handleChangeNumber = (event) => {
-    console.log("teclado: ", event.target.value);
     setNewNumber(event.target.value);
   };
-  const addNote = (event) => {
-    event.preventDefault();
-    console.log("botón: ", event.target);
-    const finded = persons.find((person) => person.name === newName.trim() || person.number === newNumber.trim());
-    console.log(
-      "%cpart2phonebooksrcApp.jsx:24 finded",
-      "color: #26bfa5;",
-      finded
+
+  const handleChangePerson = (event) => {
+    const searchTerm = event.target.value.toLowerCase(); //insensible a mayusculas o minusculas
+    setSearchPerson(searchTerm); //usamos searchTerm y no searchPerson, xq estamos dentro del controlador por lo que el val del 2do es uno menor al real
+
+    //TODO Filtro por nombres
+    const filteredPersons = allPersons.filter((person) =>
+      person.name.toLowerCase().includes(searchTerm) //todas las que incluyan el seacTerm
     );
-    let newObj = { id: persons.length + 1, name: newName.trim(), number: newNumber.trim() };
-    finded != undefined
-      ? alert(`The name ${newName} or the phone number ${newNumber} is already added to phonebook`)
-      : setPersons(persons.concat(newObj));
+    console.log("filteredPersons: ", filteredPersons);
+    setPersons(filteredPersons); //Persons == filteredPersons
+  };
+
+  const handleSumbmitAddNote = (event) => {
+    event.preventDefault();
+    const finded = allPersons.find(
+      (person) =>
+        person.name === newName.trim() || person.number === newNumber.trim()
+    );
+    let newObj = {
+      id: allPersons.length + 1,
+      name: newName.trim(),
+      number: newNumber.trim(),
+    };
+    if (finded) {
+      alert(
+        `The name ${newName} or the phone number ${newNumber} is already added to phonebook`
+      );
+    } else {
+      const updatedPersons = allPersons.concat(newObj);
+      setAllPersons(updatedPersons); //se añade a AllPersons
+      setPersons(updatedPersons); //como es una copia, tambien en persons
+    }
     setNewName("");
     setNewNumber("");
   };
@@ -35,7 +60,15 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addNote}>
+      <form>
+        {/* Se maneja el valor del input con un state u un controlador de eventos */}
+        <div>
+          Filtro por nombre:{" "}
+          <input value={searchPersons} onChange={handleChangePerson} />
+        </div>
+        <br />
+      </form>
+      <form onSubmit={handleSumbmitAddNote}>
         <div>
           name: <input value={newName} onChange={handleChangeName} />
         </div>
@@ -47,6 +80,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
+      {/* Se muestra person, su estado inicial === All persons, con filtro va cambiando hasta que se añada nueva nota y se muestre todo el contenido */}
       {persons.map((person) => (
         <Note key={person.id} person={person} />
       ))}
--- part2/phonebook/src/components/Note.jsx
+++ part2/phonebook/src/components/Note.jsx
@@ -1,10 +1,9 @@
 const Note = (props) => {
-  console.log(
-    "%cpart2phonebooksrcApp.jsx:4 props Note",
-    "color: #007acc;",
-    props.person
+  return (
+    <p>
+      {props.person.name} {props.person.number}
+    </p>
   );
-  return <p>{props.person.name} {props.person.number}</p>;
 };
 
 export default Note;
